ddb8ca24b7b4cccb90f173a8fbaf3e878caab9dc
Husam Al-Dhaheri <hossamaldahry@gmail.com>
2026-02-16 23:11:33 +0300

    feat: Add Cloudinary hostname to Next.js image remote patterns.

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
